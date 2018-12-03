4fe39e3e286a7b970cb1667837b1056726177116
Rafael Natali <40840504+rafaelmnatali@users.noreply.github.com>
2018-12-03 16:54:42 -0600

    Added "CLICK" to set options
    Added "CLICK" to set options

--- aws/s3.py
+++ aws/s3.py
@@ -1,16 +1,19 @@
 import boto3
 import botocore
+import click
 
-s3 = boto3.resource('s3')
+@click.command()
+@click.option("--acl", help="select TRUE to list ACL for all buckets")
 
-for bucket in s3.buckets.all():
- 	try:
- 		bucket_acl  	= s3.BucketAcl(bucket.name)
-# 		bucket_policy  	= s3.BucketPolicy(bucket.name)
+def acl(acl):
+	if acl == "TRUE":
+		s3 = boto3.resource('s3')
+		for bucket in s3.buckets.all():
+			try:
+				bucket_acl = s3.BucketAcl(bucket.name)
+				print("Bucket:", bucket.name, ", ACL: ", bucket_acl.load())
+			except botocore.exceptions.ClientError as error:
+				pass
 
- 		print("Bucket:", bucket.name, "ACL: ", bucket_acl.load())#, "Policies:", bucket_policy.load())
-
- 	except botocore.exceptions.ClientError as error:
- 		pass
-
-
+if __name__ == '__main__':
+	acl()
