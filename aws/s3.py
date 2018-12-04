03a0f67b20af9183fb751a8a351fac347075113a
Rafael Natali <40840504+rafaelmnatali@users.noreply.github.com>
2018-12-03 23:28:19 -0600

    Updated s3.py
    updated functions:
    
    S3 listbuckets
    S3 getacl

--- aws/s3.py
+++ aws/s3.py
@@ -1,19 +1,43 @@
 import boto3
-import botocore
 import click
+from prettytable import PrettyTable
 
-@click.command()
-@click.option("--acl", help="select TRUE to list ACL for all buckets")
 
-def acl(acl):
-	if acl == "TRUE":
-		s3 = boto3.resource('s3')
-		for bucket in s3.buckets.all():
-			try:
-				bucket_acl = s3.BucketAcl(bucket.name)
-				print("Bucket:", bucket.name, ", ACL: ", bucket_acl.load())
-			except botocore.exceptions.ClientError as error:
-				pass
+# Retrieve s3 information
+s3 = boto3.resource('s3')
+
+@click.group()
+def buckets():
+	"""Command for S3"""
+
+@buckets.command('listbucket')
+@click.option('--listbucket', default="true", help="List S3 bucket names")
+
+def list_bucket(listbucket):
+	x = PrettyTable()
+	x.field_names = ["S3 Bucket"]
+	x.align["S3 Bucket"] = "l"
+
+	for bucket in s3.buckets.all():
+		x.add_row([bucket.name])
+	print(x)
+	return
+
+
+@buckets.command('getacl')
+@click.option('--getacl', default="true", help="List S3 bucket's ACL information")
+
+def acl_bucket(getacl):
+	x = PrettyTable()
+	x.field_names = ["S3 Bucket", "ACL"]
+
+	for bucket in s3.buckets.all():
+		bucket_acl = s3.BucketAcl(bucket.name)
+	#	print("Bucket:", bucket.name, "| ACL: ", bucket_acl.load())
+		x.add_row([bucket.name,bucket_acl.load])
+	print(x)
+	return
+    
 
 if __name__ == '__main__':
-	acl()
+	buckets()
