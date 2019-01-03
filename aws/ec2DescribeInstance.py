b6b2a3a659b7c74ce312262c0cd7101ee2070507
Rafael Natali <rafael@centaurosolutions.com>
2019-01-03 15:59:57 -0600

    updating ec2DescribeInstance.py

--- aws/ec2DescribeInstance.py
+++ aws/ec2DescribeInstance.py
@@ -1,19 +1,21 @@
 import boto3
 
-ec2 = boto3.client('ec2')
+# Enter the region your instances are in. Include only the region without specifying Availability Zone; e.g., 'us-east-1'
+region = 'us-west-2'
 
-response = ec2.describe_instances(
+ec2 = boto3.client('ec2', region_name=region)
+ec2_describe = ec2.describe_instances(
     Filters=[
         {
-            'Name': 'tag:Name',
+            'Name': 'tag:Restart',
             'Values': [
-                'condofacil-dev',
+                'yes',
             ]
         },
     ]
 )
 
-for r in response['Reservations']:
-	for i in (r['Instances']):
-		print(i['InstanceId'])
-
+for r in ec2_describe['Reservations']:
+    for i in (r['Instances']):
+        instances_id = (i['InstanceId'])
+        print(instances_id)
